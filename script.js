406a50e7d7be8d8e80fa64352d2830bba4ec9c5c
AdorkablyNicole <hello@nicolegeorge.me>
2020-07-21 21:27:00 +0930

    added JQuery ui elements after prompt.

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var magic8Ball = {};
+  magic8Ball.listOfAnswers = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+  ];
 
-var magic8Ball = {};
-magic8Ball.listOfAnswers = [
-  'It is certain',
-  'It is decidedly so',
-  'Without a doubt',
-  'Yes definitely',
-  'Reply hazy try again',
-  'Ask again later', 
-  'Better not tell you now',
-  'My sources say no',
-  'Outlook not so good',
-  'Very doubtful',
-];
-// define the method
-magic8Ball.askQuestion = function (question) {
-  // create a random number
-  var randomNumber = Math.random();
+  // define the method
+  $('#answer').fadeIn(4000);
+  magic8Ball.askQuestion = function (question) {
+    // create a random number
+    var randomNumber = Math.random();
 
-  // make the random number between 0 and the number of items in your array
-  var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+    // make the random number between 0 and the number of items in your array
+    var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
 
-  // round down the number to be a whole number
-  var randomIndex = Math.floor(randomNumberForListOfAnswers);
+    // round down the number to be a whole number
+    var randomIndex = Math.floor(randomNumberForListOfAnswers);
 
-  // use that number to index a random number from the answers array
-  var answer = this.listOfAnswers[randomIndex];
+    // use that number to index a random number from the answers array
+    var answer = this.listOfAnswers[randomIndex];
 
-  console.log(question);
-  console.log(answer);
-};
-var randomDance = magic8Ball.askQuestion('Will I get a new job?');
+    $('#answer').text(answer);
 
+    console.log(question);
+    console.log(answer);
+  };
+//   var randomDance = magic8Ball.askQuestion('Will I get a new job?');
 
-var onClick = function() {
-    var question = prompt("ASK A YES/NO QUESTION!");
+  var onClick = function () {
+    $('#answer').hide();
+    var question = prompt('ASK A YES/NO QUESTION!');
     magic8Ball.askQuestion(question);
-};
+    $('#answer').hide();
+    $('#8ball').effect('shake');
+    $('#answer').fadeIn(4000);
+    $('#8ball').attr(
+      'src',
+      'https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png'
+    );
+  };
 
-$("#questionButton").click(onClick);
-
-
-
-
-
+  //wait half a second before showing prompt
+  setTimeout(function () {
+    //show prompt
+    var question = prompt('Ask a yes or no question');
+    magic8Ball.askQuestion(question);
+  }, 500);
+  $('#questionButton').click(onClick);
 });
